1a42b419e073e2123ea5091e4f9edc3dbc788310
Sayan Das <poke19962008@gmail.com>
2016-01-13 18:16:01 +0530

    converted folder to tar balls

--- nodeServer.js
+++ nodeServer.js
@@ -1,13 +1,48 @@
 var express  = require('express');
 var fs = require('fs');
+var path = require('path');
+var targz = require('tar.gz');
+var rmdir = require('rimraf');
 
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/getFileList', function(req, res){
-	var foo = fs.readdirSync(__dirname + "/data");
-	res.send(foo);
+	var fl = fs.readdirSync(__dirname + "/data");
+
+	for(var i =0; i<fl.length; i++){
+		var ext =	path.extname(__dirname + "/data/" + fl[i]);
+
+		if(ext == ""){
+			targz().createReadStream('./data/' + fl[i]).pipe(fs.createWriteStream('./data/'+ fl[i] +'.tar.gz'));
+			rmdir(__dirname + "/data/" + fl[i], function(err){
+				console.log('[ERROR] Cannot delete ' + fl[i]);
+			});
+			fl[i] = fl[i] +'.tar.gz';
+		}
+	}
+	res.send(fl);
+
+});
+
+
+app.get('/downloadFile', function (req, res){
+	var q = req.query;
+	res.download(__dirname + "/data/" + q.file, q.file, function (err) {
+		if(err)	console.log(err);
+	});
+
+});
+
+app.get('/deleteFile', function (req, res){
+	var q = req.query;
+	fs.unlink(__dirname + "/data/" + q.file, function (err){
+		if(err)
+			res.send('error');
+		else
+			res.send('success');
+	});
 
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,24 @@ $(document).ready(function(){
   .done(function(fileList){
     for(var i=0; i<fileList.length; i++){
       var index = i+1;
-      $("#list_content").append("<tr><td>"+ index +"</td><td class=\"fileName\">"+ fileList[i] +"</td><td><button type=\"button\" class=\" download btn btn-default btn-sm\" name=\"button\"><span class=\"glyphicon glyphicon-download-alt\" aria-hidden=\"true\"></span></button></td><td><button type=\"button\" class=\"delete btn btn-default btn-sm\" name=\"button\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></button></td></tr>");
+      $("#list_content").append("<tr><td>"+ index +"</td><td>"+ fileList[i] +"</td><td><a target=\"_blank\" href=\"downloadFile?file="+ fileList[i] +"\"><span class=\"glyphicon glyphicon-download-alt\" aria-hidden=\"true\"></span></a></td><td><a id=\"remove\" href=\"#\" alt=\""+ fileList[i] +"\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></a></td></tr>");
     }
+
+    $('#remove').click(function (){
+      $.ajax({
+        url: '/deleteFile',
+        method:'GET',
+        data: {
+          file: $(this).attr('alt'),
+        }
+      })
+      .done(function (msg){
+        if(msg == 'success')
+          location.reload(true);
+        else
+          alert('Error deleting file. Try again later.');
+      });
+    });
   });
+
 });
